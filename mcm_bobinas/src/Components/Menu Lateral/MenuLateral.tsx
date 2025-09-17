d2a2af2baf4e407603c4d72c73362eab2ab7739c
IsabelliLuz <ramalholuzisa@gmail.com>
2025-09-16 22:15:32 -0300

    pag Simular Pedido e menu lateral

--- mcm_bobinas/src/Components/Menu Lateral/MenuLateral.tsx
+++ mcm_bobinas/src/Components/Menu Lateral/MenuLateral.tsx
@@ -1,19 +1,48 @@
+import { NavBar } from '../NavBar/NavBar'; // Importe a NavBar uma única vez
+import logonome from "../../assets/logonome.png"
+import upload from "../../assets/Upload.png"
+import './MenuLateral.css'; 
+
+
 export function MenuLateral() {
-    return(
-        <section className="menu-container">
-            <div className="lateral">
-                <div className="logo">
+  return (
+    <div className="container">
+      <div className="nav">
+        <NavBar />
+      </div>
+      
+      
+      <section className="menu-lateral-container">
+       {/* Seção do Menu */}
+        <nav className="menu-section">
+        {/* Seção do Logo */}
+      <div className="logo-section">
+        <img src={logonome} alt="MCM Bobinas Logo" className="logo" />
+      </div>
+        <a href="#" className="menu-item">
+          Adicionar produto
+        </a>
+        <br></br>
+        <a href="#" className="menu-item">
+          Adicionar nota fiscal
+        </a>
+        <br></br>
+        <a href="#" className="menu-item">
+          Simulação pedido
+        </a>
+        <br></br><br></br><br></br><br></br><br></br>
+      </nav>
 
-                </div>
+      {/* Seção de Upload */}
+      <div className="logo-section">
+        <img src={upload} alt="MCM Bobinas Logo" className="logo" />
+      </div>
 
-                <div className="menu-list">
+      </section>
+      
+    </div>
 
-                </div>
-
-                <div className="upload-aqv">
-                    
-                </div>
-            </div>
-        </section>
-    )
+      );
 }
+
+   
--- mcm_bobinas/src/Router.tsx
+++ mcm_bobinas/src/Router.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "./Pages/HomePage";
 import { NavBar } from "./Components/NavBar/NavBar";
+import { MenuLateral } from "./Components/Menu Lateral/MenuLateral";
+import { SimulacaoPedido } from "./Components/Simulacao Pedido/SimulacaoPedido";
 
 const Router: React.FC = () => {
   return (
@@ -8,8 +10,8 @@ const Router: React.FC = () => {
       <Routes>
         {/* Home */}
         <Route path="/" element={<HomePage/>}></Route>
-
-
+        <Route path="/MenuLateral" element={<MenuLateral/>}></Route>
+        <Route path="/SimulacaoPedido" element={<SimulacaoPedido/>}></Route>
 
         {/* Components */}
         <Route path="/navbar" element={<NavBar/>}></Route>
